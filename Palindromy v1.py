a5128f465ea2d58a7802c8dc7b5e84d804dd7073
HanamiHana <95184843+HanamiHana@users.noreply.github.com>
2021-12-08 15:50:26 +0100

    Add files via upload

---
+++ Palindromy v1.py
@@ -0,0 +1,12 @@
+var=input("Wpisz wyrażenie: ")
+reverse_var=var[::-1]
+print(reverse_var)
+
+def if_palindrome():
+    if var==reverse_var:
+        print("To jest palindrom")
+    else:
+        print("To nie jest palindrom"))
+
+if_palindrome()
+
